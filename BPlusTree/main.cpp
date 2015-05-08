e5c2bea09c973daf9b08f95d3d53716e54d21295
boats1992 <boats1992@hotmail.com>
2015-05-07 22:19:45 -0400

    new

--- BPlusTree/main.cpp
+++ BPlusTree/main.cpp
@@ -1,9 +1,28 @@
-#include "BPlusTree.hpp"
-#include <string>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <pthread.h>
+#include <semaphore.h>
 
-int main()
-{
-    BPlusTree bt;
+#include "rtm.h"
+#include "BPlusTree.hpp"
+
+/* macro */
+#define NUM_THREAD 4
+#define WORK_PER_THD 1000000
+
+
+/* global variable */
+int num;
+BPlusTree bt;
+
+void worker() {
+    for (int i = 0; i < WORK_PER_THD; i++) {
+        cout << *(long*)bt.get(26) << endl;
+    }
+}
+
+int main() {
 
     long a = 123, b = 642;
 
@@ -12,8 +31,12 @@ int main()
     bt.put(26, &a);
     bt.put(29, &b);
 
-    
-    cout << *(long*)bt.get(26) << endl;
-    //cout << *(long*)bt.get(33) << endl;
-    
+    std::thread* thd[NUM_THREAD];
+    for (int i = 0; i < NUM_THREAD; i++)
+        thd[i] = new std::thread(worker);
+    for (int i = 0; i < NUM_THREAD; i++)
+        thd[i]->join();
+    //std::cout << "Final value: " << num << std::endl;
 }
+
+
